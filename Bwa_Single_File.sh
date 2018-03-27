8bf71f15cd395906b3bcc2c418a756560b9245c4
Thomas Adams <thomas.adams@emr.ac.uk>
2018-03-27 12:04:05 +0100

    Inital commit, shebang, sge info

---
+++ Bwa_Single_File.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#$ -S /bin/bash
+#$ -cwd
+#$ -pe smp 12
+#$ -l virtual_free=1G
+#$ -l h=blacklace03.blacklace|blacklace04.blacklace|blacklace05.blacklace|blacklace06.blacklace|blacklace07.blacklace|blacklace08.blacklace|blacklace09.blacklace|blacklace10.blacklace
