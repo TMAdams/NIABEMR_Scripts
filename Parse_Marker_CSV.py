e962dd552e7efb9b450107b520ff7ece14701f95
Thomas Adams <thomas.adams@emr.ac.uk>
2018-05-25 12:42:25 +0100

    Modifies commands to take multiple inputs

--- Parse_Marker_CSV.py
+++ Parse_Marker_CSV.py
@@ -14,8 +14,8 @@ import argparse
 import csv
 
 ap = argparse.ArgumentParser()
-ap.add_argument('--input', required=True, type=str, help='Input csv for \
-                parsing')
+ap.add_argument('--inputs', required=True, type=str, nargs='+', help='List of \
+                input csvs for parsing')
 ap.add_argument('--output', required=True, type=str, help='Filename to output \
                 parsed file to')
 conf = ap.parse_args()
@@ -25,19 +25,22 @@ conf = ap.parse_args()
 # Perform parsing and write out file
 # -----------------------------------------------------
 
-with open(conf.input) as f:
-    with open(conf.output, 'w') as o:
-        reader = csv.reader(f)
-        next(reader, None)
-        lines = list(reader)
-        for line in lines:
-            top_line_1 = line[0]
-            top_line_2 = line[2:4]
-            bottom_line = line[6:]
-            top_2_string = "\t".join(top_line_2)
-            top = "\t".join([top_line_1, top_2_string])
-            bottom = "\t".join(bottom_line)
-            o.write(top)
-            o.write("\n")
-            o.write(bottom)
-            o.write("\n")
+Input_List = conf.inputs
+
+for Input in Input_List:
+    with open(Input) as f:
+        with open(conf.output, 'w') as o:
+            reader = csv.reader(f)
+            next(reader, None)
+            lines = list(reader)
+            for line in lines:
+                top_line_1 = line[0]
+                top_line_2 = line[2:4]
+                bottom_line = line[6:]
+                top_2_string = "\t".join(top_line_2)
+                top = "\t".join([top_line_1, top_2_string])
+                bottom = "\t".join(bottom_line)
+                o.write(top)
+                o.write("\n")
+                o.write(bottom)
+                o.write("\n")
