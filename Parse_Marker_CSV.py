cccb45bede97007acb6ca16458af743ec70d5d49
Thomas Adams <thomas.adams@emr.ac.uk>
2018-05-25 12:33:33 +0100

    Adds commands to perform parsing and write out file

--- Parse_Marker_CSV.py
+++ Parse_Marker_CSV.py
@@ -11,6 +11,7 @@ to a format for MQM
 # -----------------------------------------------------
 
 import argparse
+import csv
 
 ap = argparse.ArgumentParser()
 ap.add_argument('--input', required=True, type=str, help='Input csv for \
@@ -23,3 +24,20 @@ conf = ap.argparse()
 # Step 2
 # Perform parsing and write out file
 # -----------------------------------------------------
+
+with open(conf.input) as f:
+    with open(conf.output, 'w') as o:
+        reader = csv.reader(f)
+        next(reader, None)
+        lines = list(reader)
+        for line in lines:
+            top_line_1 = line[0]
+            top_line_2 = line[2:4]
+            bottom_line = line[6:]
+            top_2_string = "\t".join(top_line_2)
+            top = "\t".join([top_line_1, top_2_string])
+            bottom = "\t".join(bottom_line)
+            o.write(top)
+            o.write("\n")
+            o.write(bottom)
+            o.write("\n")
