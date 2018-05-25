2bc64fd78576c2cbb672b47f9afad23cc2c87d16
Thomas Adams <thomas.adams@emr.ac.uk>
2018-05-25 12:02:13 +0100

    Adds title for step 1

--- Parse_Marker_CSV.py
+++ Parse_Marker_CSV.py
@@ -5,4 +5,9 @@ Script written for Charlotte to parse a csv of marker information
 to a format for MQM
 '''
 
+# -----------------------------------------------------
+# Step 1
+# Load modules and parse input files
+# -----------------------------------------------------
+
 import argparse
